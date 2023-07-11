64b5843970e089b70ce9a24f9015d4d8faab0cc4
AntonioAiosa <aiosaantonio@hotmail.com>
2023-07-11 18:19:16 +0200

    added some coordinates

--- src/MapGraphics.js
+++ src/MapGraphics.js
@@ -1,29 +1,96 @@
-import {useEffect} from "react";
-
+import { useEffect, useState,  useRef} from "react";
 import Graphic from "@arcgis/core/Graphic";
 
+import Faker from "faker";
+
 const MapGraphics = ({view}) => {
-    useEffect (() =>{
-        const graph = new Graphic({
-            geometry:{
-                type:"point",
-                longitude:12.4963655, 
-                latitude: 41.9027835
-        },
-        symbol:{
-            type:"simple-marker",
-            color:[226, 119, 40],
-            size: 5,
-            outline: {
-                color: [255, 255, 255], // White
-                width: 1
-            }
-    }  
-        });
+    const formRef = useRef();
+    const [state, setState] = useState({
+        name: "",
+        address: "",
+      });
+      Faker.locale = "ar";
     
-        view.graphics.add(graph)
-      
-        }, [view.graphics]);
+      let locations = [     {
+        longitude:7.666065, 
+        latitude: 45.097283
+    },
+    {
+        longitude:7.635756, 
+        latitude: 45.083461
+    },
+    {
+        longitude:7.677794, 
+        latitude: 45.101055
+    },
+    {
+        longitude:12.98114309, 
+        latitude: 46.06005178
+    },
+    {
+        longitude:13.54822195, 
+        latitude: 42.08483293
+    },
+    {
+        longitude:13.31540594, 
+        latitude: 46.44804944
+    },
+    {
+        longitude:7.85656384, 
+        latitude: 45.4608501
+    },
+
+];
+    //   for (let index = 0; index < 10; index++)
+    //     locations.push({
+    //       name: Faker.name.findName(),
+    //       address: Faker.address.streetAddress(),
+    //       latitude: Faker.address.latitude(),
+    //       longitude: Faker.address.longitude(),
+    //     });
+        useEffect(() => {
+            const glResult1 = view.map.findLayerById("glResult1");
+        
+            if (glResult1) {
+              locations.map((location) => {
+                const graph = new Graphic({
+                  geometry: {
+                    type: "point",
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                  },
+                  symbol: {
+                    type:"simple-marker",
+                    // color:[226, 119, 40], orange
+                    color: "blue",
+                    size: 8,
+                    outline: {
+                        color: [255, 255, 255], // White
+                        width: 1
+                    }
+                  },
+                  attributes: {
+                    title: location.name,
+                    content: location.address,
+                  },
+                });
+                glResult1.add(graph);
+            });
+        }
+        
+        
+            // view.ui.add(formRef.current, "top-right");
+            // view.on("click", (event) => {
+            //   view.hitTest(event).then((resp) => {
+            //     if (resp.results.length > 0) {
+            //       setState({
+            //         name: resp.results[0].graphic.attributes.title,
+            //         address: resp.results[0].graphic.attributes.content,
+            //       });
+            //     }
+            //   });
+            // });
+          }, [view.map.findLayerById("glResult1")]);
             
         
 
--- src/MyMap.js
+++ src/MyMap.js
@@ -4,18 +4,31 @@ import MapView from "@arcgis/core/views/MapView"
 import { useEffect, useRef, useState } from "react";
 import MapWidgets from "./MapWidgets";
 import MapGraphics from "./MapGraphics";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 
 const MyMap = () => {
 const mapRef = useRef(null);
 const [view, setView] = useState(null)
-useEffect (() =>{
+
+const glResult1 = new GraphicsLayer({
+    id: "glResult1",
+  });
+
+  const glResult2 = new GraphicsLayer({
+    id: "glResult2",
+  });
+
+useEffect (() =>{ 
 new MapView({
     container: mapRef.current,
     map: new Map({
-        basemap:"streets",
+        // basemap:"streets", realistica
+        basemap:"dark-gray-vector",
+        layers: [glResult1, glResult2],
     }),
-    center: [12.4963655, 41.9027835], // longitudine,latitudine
-    zoom: 6,
+    // center: [7.68685655,45.070312],  longitudine,latitudine Torino 
+    center: [12.4963655, 41.9027835], // longitudine,latitudine Italia
+    zoom: 4,
 }).when((view) => setView(view))
 }, []);
 
